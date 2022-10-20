7c9e4f9985a9a4925eacc9f08c8d4cd472c28c29
ManuelBraimllari <manubra999@gmail.com>
2022-10-20 09:57:48 +0200

    finish

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let name = prompt (Dimmi il tuo nome)
+let surname = prompt (Dimmi il tuo cognome)
+let favouritecolor = prompt (dimmi il tuo colore preferito)
+let valore = '$(name)&(surname)&(favouritecolor)22'
+document.getElementById ('password').innerHTML = valor
