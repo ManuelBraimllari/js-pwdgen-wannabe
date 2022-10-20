f1a86dcb4716fa098fd5566faf85216a7dca0474
ManuelBraimllari <manubra999@gmail.com>
2022-10-20 10:18:47 +0200

    correzioni

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-let name = prompt (Dimmi il tuo nome)
-let surname = prompt (Dimmi il tuo cognome)
-let favouritecolor = prompt (dimmi il tuo colore preferito)
-let valore = '$(name)&(surname)&(favouritecolor)22'
-document.getElementById ('password').innerHTML = valor
+let name = prompt ('Dimmi il tuo nome');
+let surname = prompt ('Dimmi il tuo cognome')
+let favouritecolor = prompt ('Dimmi il tuo colore preferito')
+let password = name + surname + favouritecolor + '22'
+window.document.getElementById('password').innerHTML = password
